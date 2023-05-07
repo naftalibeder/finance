68a99ed1c99a1acefd180e24cbc989f1049a7510
Naftali Beder <naftali@naftalibeder.com>
2023-05-06 22:39:10 -0500

    Add custom font and improve spacing

--- client/index.ts
+++ client/index.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import * as eta from "eta";
 import db from "../db";
+import { Price } from "../types";
 
 const renderHome = () => {
   const template = fs.readFileSync(path.resolve(__dirname, "home.eta"), {
@@ -13,6 +14,11 @@ const renderHome = () => {
   const data = {
     accounts,
     transactions,
+    formatCurrency: (a: Price) =>
+      new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: a.currency,
+      }).format(a.amount),
   };
 
   const home = eta.render(template, data);
