88cf3af03b54275990e87761827d57f3ff975977
Naftali Beder <naftali@naftalibeder.com>
2023-08-29 22:20:10 -0500

    Fix search and add find-one function

--- extractor/utils/browser.ts
+++ extractor/utils/browser.ts
@@ -1,28 +1,62 @@
-import { Frame, Locator, Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export const findAll = async (
   page: Page,
-  selector: string
+  selector: string,
+  options?: {
+    timeout?: number;
+  }
 ): Promise<Locator[]> => {
+  const frames = page.frames();
   let res: Locator[] = [];
-  let i = 0;
 
-  while (true) {
+  console.log(
+    `Searching for selector '${selector}' in ${frames.length} frames`
+  );
+
+  for (let i = 0; i < frames.length; i++) {
+    const frame = frames[i];
     try {
-      const frame = page.frames()[i];
-      if (!frame) {
-        break;
-      }
-
       const loc = frame.locator(selector);
-      const resIter = await loc.all();
-      res = [...res, ...resIter];
+      await loc.waitFor({
+        state: "visible",
+        timeout: options?.timeout ?? 2000,
+      });
+      res = [...res, loc];
     } catch (e) {
-      console.log(`No results in frame ${i}`);
+      // No results; continue searching.
     }
-
-    i++;
   }
 
+  if (res.length === 0) {
+    console.log(`Found no elements for selector '${selector}'`);
+    return [];
+  }
+
+  const texts: string[] = [];
+  for (const r of res) {
+    const t = await r.textContent();
+    if (t) {
+      texts.push(t);
+    }
+  }
+  console.log(
+    `Found ${res.length} elements for selector '${selector}': ${texts}`
+  );
   return res;
+};
+
+export const findFirst = async (
+  page: Page,
+  selector: string,
+  options?: {
+    timeout?: number;
+  }
+): Promise<Locator | undefined> => {
+  const all = await findAll(page, selector, options);
+  if (all.length === 0) {
+    return undefined;
+  }
+
+  return all[0];
 };
