6d81714c28bec3601fd1251952a31723c6db3257
Naftali Beder <naftali@naftalibeder.com>
2023-08-29 22:21:30 -0500

    Add test for non-existing element

--- extractor/utils/browser_test.ts
+++ extractor/utils/browser_test.ts
@@ -1,31 +1,42 @@
 import { firefox } from "@playwright/test";
 import mocha from "mocha";
-import { findAll } from "./browser.js";
+import { findAll, findFirst } from "./browser.js";
 
 const { describe, it } = mocha;
+
+// TODO: Make path relative to project.
+const url = `file:///Users/naftalibeder/code/finance/extractor/utils/fixtures/iframes.html`;
 
 describe("find all elements on page by selector", () => {
   it("find existing", async () => {
     const browser = await firefox.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto(
-      `file:///Users/naftalibeder/code/finance/extractor/utils/fixtures/iframes.html` // TODO: Make path relative to project.
-    );
+    await page.goto(url);
 
-    let resultTexts: (string | null)[] = [];
-    try {
-      const selector = ".monkey";
-      const locs = await findAll(page, selector);
-      resultTexts = await Promise.all(locs.map((o) => o.textContent()));
-    } catch (e) {
-      throw e;
-    }
-
+    const selector = ".monkey";
+    const locs = await findAll(page, selector, { timeout: 1000 });
+    const resultTexts = await Promise.all(locs.map((o) => o.textContent()));
     const expectedTexts = ["I am an outer monkey!", "I am an inner monkey!"];
-
     if (JSON.stringify(expectedTexts) !== JSON.stringify(resultTexts)) {
       throw new Error(
         `Incorrect results; expected '${expectedTexts}', got '${resultTexts}'`
+      );
+    }
+
+    await browser.close();
+  }).timeout(0);
+
+  it("find non-existing", async () => {
+    const browser = await firefox.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const selector = ".not-monkey";
+    const loc = await findFirst(page, selector, { timeout: 1000 });
+    const resultText = await loc?.textContent();
+    if (loc) {
+      throw new Error(
+        `Incorrect results; expected no results, got '${resultText}'`
       );
     }
 
