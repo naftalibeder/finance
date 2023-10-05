074c16632f0ae03f772a43cf41c9a20e9652b5e6
Naftali Beder <naftali@naftalibeder.com>
2023-10-05 14:19:46 -0500

    Shorten documentation

--- extractor/scripts/repl.cjs
+++ extractor/scripts/repl.cjs
@@ -11,10 +11,6 @@ Usage:
 
 // Save all html to a file in `./tmp`.
 > await _.scrape(page)
-
-// If generating a test to commit to version control, be sure to manually remove
-// personally identifying information! Example expressions:
-// ".* \(\.{3}\d{4}\)"
 */
 
 const { firefox } = require("@playwright/test");
@@ -47,6 +43,6 @@ module.exports.scrape = async (page) => {
 
   console.log(`Wrote html of length ${allHtml.length} to ${path}`);
   console.log(
-    `Attempted to redact personally identifying information, but please verify the file does not contain any more private data.`
+    `Please verify the file does not contain any private data like account names or numbers.`
   );
 };
